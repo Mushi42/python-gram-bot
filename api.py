1db5be329da7501aa56c2016cf07cea238874381
Musharaf Faheem <musharaf42@gmail.com>
2023-05-04 01:55:34 +0500

    new apis added

--- api.py
+++ api.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 client = MongoClient('mongodb://localhost:27017/')
 db = client['scrapper']
 collection = db['commissioners']
+organizationsCollection = db['organizations']
+rolesCollection = db['roles']
 
 
 # Define route to retrieve commissioner data
@@ -17,13 +19,37 @@ def get_commissioners():
         commissioners.append({
             'name': commissioner['name'],
             'role': commissioner['role'],
-            'company': commissioner['company'],
+            'organization': commissioner['organization'],
+            'image': commissioner['image'],
+            'profileLink': commissioner['profileLink'],
             'address': commissioner['address'],
             'address2': commissioner['address2'],
             'myTeamsLink': commissioner['myTeamsLink'],
             'team': commissioner['team']
         })
     return jsonify(commissioners)
+
+
+@app.route('/roles', methods=['GET'])
+def get_roles():
+    roles = []
+    for role in rolesCollection.find():
+        print(role)
+        roles.append({
+            'name': role['name']
+        })
+    print(roles)
+    return jsonify(roles)
+
+
+@app.route('/organizations', methods=['GET'])
+def get_organztions():
+    organizations = []
+    for organization in organizationsCollection.find():
+        organizations.append({
+            'name': organization['name']
+        })
+    return jsonify(organizations)
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pymongo
 from pymongo import MongoClient
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -7,7 +6,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 client = MongoClient('mongodb://localhost:27017')
 db = client['scrapper']
-collection = db['commissioners']
+commissionersCollection = db['commissioners']
+organizationsCollection = db['organizations']
+rolesCollection = db['roles']
 
 BASE_URL = "https://commissioners.ec.europa.eu"
 
@@ -38,7 +39,7 @@ commissionersList = [{
     'profileLink': commissioner.find_element(By.CLASS_NAME, 'ecl-link').get_attribute('href'),
     'role': commissioner.find_element(By.CLASS_NAME, 'ecl-content-block__primary-meta-item').text,
     'name': commissioner.find_element(By.CLASS_NAME, 'ecl-content-block__title').text,
-    'company': commissioner.find_element(By.CLASS_NAME, 'ecl-content-block__description').text,
+    'organization': commissioner.find_element(By.CLASS_NAME, 'ecl-content-block__description').text,
 } for commissioner in commissioners]
 
 # Extract additional information for each commissioner
@@ -46,22 +47,25 @@ for commissioner in commissionersList:
     driver.get(commissioner['profileLink'])
 
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "ecl-list-illustration__description")))
+    wait.until(EC.visibility_of_element_located(
+        (By.CLASS_NAME, "ecl-list-illustration__description")))
 
-    addressDiv = driver.find_element(By.CLASS_NAME, 'ecl-list-illustration__description')
+    addressDiv = driver.find_element(
+        By.CLASS_NAME, 'ecl-list-illustration__description')
     pTags = addressDiv.find_elements(By.TAG_NAME, 'p')
     commissioner['address'] = pTags[1].text
 
     myTeamDiv = driver.find_elements(By.CLASS_NAME, 'ecl-link--secondary')
     commissioner['myTeamsLink'] = myTeamDiv[1].get_attribute('href')
-
+    
 teamMembers = []
 memberObject = {}
 for commissioner in commissionersList:
     driver.get(commissioner['myTeamsLink'])
 
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "ecl-featured-item__item")))
+    wait.until(EC.visibility_of_element_located(
+        (By.CLASS_NAME, "ecl-featured-item__item")))
 
     addressDiv = driver.find_elements(By.CLASS_NAME, 'ecl-featured-item__item')
     if len(addressDiv) > 1:
@@ -70,10 +74,13 @@ for commissioner in commissionersList:
         pTags = addressDiv[0].find_elements(By.TAG_NAME, 'p')
     commissioner['address2'] = pTags[1].text
 
-    teamsDiv = driver.find_elements(By.CLASS_NAME, 'ecl-content-item-block__item')
+    teamsDiv = driver.find_elements(
+        By.CLASS_NAME, 'ecl-content-item-block__item')
     for member in teamsDiv:
-        memberObject['title'] = member.find_element(By.CLASS_NAME, 'ecl-content-block__title').text
-        userDetails = member.find_elements(By.CLASS_NAME, 'ecl-description-list__definition')
+        memberObject['title'] = member.find_element(
+            By.CLASS_NAME, 'ecl-content-block__title').text
+        userDetails = member.find_elements(
+            By.CLASS_NAME, 'ecl-description-list__definition')
         if len(userDetails) > 0:
             memberObject['email'] = userDetails[0].text
         if len(userDetails) > 1:
@@ -81,7 +88,8 @@ for commissioner in commissionersList:
         if len(userDetails) > 2:
             memberObject['responsibilties'] = userDetails[2].text
 
-        memberObject['image'] = member.find_element(By.CLASS_NAME, 'ecl-content-item__image').get_attribute('src')
+        memberObject['image'] = member.find_element(
+            By.CLASS_NAME, 'ecl-content-item__image').get_attribute('src')
 
         teamMembers.append(memberObject)
         memberObject = {}
@@ -89,14 +97,27 @@ for commissioner in commissionersList:
     commissioner['team'] = teamMembers
     teamMembers = []
     memberObject = {}
-
 # Close the webdriver
 driver.quit()
 
 # Print the list of commissioners
-print(commissionersList)
+
 for commissioner in commissionersList:
+    # Update or create the organization document
+    query = {'name': commissioner['organization']}
+    update = {'$set': {"name": commissioner['organization']}}
+    org = organizationsCollection.update_one(query, update, upsert=True)
+    
+    # Update or create the role document
+    query = {'name': commissioner['role']}
+    update = {'$set': {"name": commissioner['role']}}
+    role = rolesCollection.update_one(query, update, upsert=True)
+
+    # Update or create the commissioner document
     query = {'name': commissioner['name']}
     update = {'$set': commissioner}
-    collection.update_one(query, update, upsert=True)
+    commissionersCollection.update_one(query, update, upsert=True)
+
+
+
 client.close()
